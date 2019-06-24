fb1a320e979e1d15acfbe3efde3b7b8dd58eaee5
necmettindemir <necmettindemir@yahoo.com>
2019-06-24 05:07:56 +0300

    ..

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,9 +4,18 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+
+const todos = [
+    {text:"first"},
+    {text:"second"}    
+];
+
 beforeEach( (done) => {
     Todo.remove({}).then( ()=> {
-        done();
+        //done();
+        return Todo.insertMany(todos);
+    }).then( () => { 
+        done(); 
     });
 });
 
@@ -29,8 +38,8 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then( (todos) => {
-                    expect(todos.length).toBe(1);
-                    expect(todos[0].text).toBe(text);
+                    expect(todos.length).toBe(3);
+                    expect(todos[2].text).toBe(text);
                     done();
                 }).catch(  (e) => done(e) );
                     
@@ -51,12 +60,25 @@ describe('POST /todos', () => {
             }
 
             Todo.find().then( (todos) => {
-                expect(todos.length).toBe(0);                
+                expect(todos.length).toBe(2);                
                 done();
             }).catch(  (e) => done(e) );
                 
         });
     });
     
+});
 
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect( (res) => {
+            expect(res.body.todos.length).toBe(2); 
+            })
+            .end(done);
+    });
 });
